689330c8452d669631e86cea0fde7125a104d25a
Joorb <joorb411@gmail.com>
2025-09-16 00:44:19 -0300

    Adicionando alguns detalhes no program.cs

--- ProjetoLoja/Program.cs
+++ ProjetoLoja/Program.cs
@@ -5,19 +5,31 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+// 1. Configurar os serviços de sessão
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30); // Tempo máximo de inatividade antes da sessão expirar
+    options.Cookie.HttpOnly = true; // Garante que o cookie só pode ser acessado pelo servidor (não por scripts no navegador)
+    options.Cookie.IsEssential = true; // Define o cookie como essencial para o funcionamento da aplicação
+});
+
+// REGISTRAR A CONNECTION STRING COMO UM SERVIÇO STRING AQUI
+builder.Services.AddSingleton<string>(builder.Configuration.GetConnectionString("DefaultConnection")!);
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-}
-app.UseStaticFiles();
 
-app.UseRouting();
+    app.UseRouting();
 
-app.UseAuthorization();
+    // ESTA LINHA É CRUCIAL E DEVE VIR ANTES DE app.UseAuthorization() ou app.MapControllerRoute() <<<
+    app.UseSession();
 
-app.MapControllerRoute(
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
+    app.Run();
+}
