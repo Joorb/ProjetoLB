ad5042671307f8bda91f7134189b5368e7ee32dd
Joorb <joorb411@gmail.com>
2025-09-16 01:30:02 -0300

    Adicionando os atributos da classe ItemCarrinho

--- ProjetoLoja/Models/ItemCarrinho.cs
+++ ProjetoLoja/Models/ItemCarrinho.cs
@@ -1,6 +1,14 @@
-﻿namespace ProjetoLoja.Models
+﻿using ProjetoLoja.Repositorio;
+
+namespace ProjetoLoja.Models
 {
     public class ItemCarrinho
     {
+        public int ProdutoId { get; set; }
+        public ProdutoRepositorio Produto { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Preco { get; set; }
+
+        public decimal Total => Quantidade * Preco;
     }
 }
