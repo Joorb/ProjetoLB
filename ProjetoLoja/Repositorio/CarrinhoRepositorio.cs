6c109a8ee1794d60f066f31653fd1143cf6c78eb
Joorb <joorb411@gmail.com>
2025-09-16 01:23:47 -0300

    Adicionando os atributos no CarrinhoRepositorio

--- ProjetoLoja/Repositorio/CarrinhoRepositorio.cs
+++ ProjetoLoja/Repositorio/CarrinhoRepositorio.cs
@@ -1,6 +1,14 @@
-﻿namespace ProjetoLoja.Repositorio
+﻿using ProjetoLoja.Models;
+
+namespace ProjetoLoja.Repositorio
 {
     public class CarrinhoRepositorio
     {
+        public int ProdutoId { get; set; }
+        public ProdutoRepositorio Produto { get; set; }
+        public int Quantidade { get; set; }
+        public decimal Preco { get; set; }
+
+        public decimal Total => Quantidade * Preco;
     }
 }
