682613e1764bb11a4faabf031d32cc04653c5600
Joorb <joorb411@gmail.com>
2025-09-16 02:03:39 -0300

    Fazendo insert e adicionando as imagens

--- ProjetoLoja/Models/ItemCarrinho.cs
+++ ProjetoLoja/Models/ItemCarrinho.cs
@@ -5,7 +5,7 @@ namespace ProjetoLoja.Models
     public class ItemCarrinho
     {
         public int ProdutoId { get; set; }
-        public ProdutoRepositorio Produto { get; set; }
+        public Produto Produto { get; set; }
         public int Quantidade { get; set; }
         public decimal Preco { get; set; }
 
--- script.sql
+++ script.sql
@@ -2,8 +2,8 @@ create database EcomLoja;
 use Ecomloja;
 
 create table produtos(
- Id int primary key auto_increment,
- Nome varchar(40),
+Id int primary key auto_increment,
+Nome varchar(40),
 Descricao varchar(40),
 Preco decimal(10,2),
 ImageUrl varchar(255),
@@ -28,6 +28,14 @@ ProdutoId int,
 Quantidade int,
 PrecoUnitario decimal(10,2)
 );
+
+INSERT INTO produtos (Nome, Descricao, Preco, ImageUrl, Estoque)
+VALUES ('Game Boy Advance', 'Videogame Game Boy Advance', 799.99, '/img/prod1.jpg', 10),
+('Labubu', 'Boneco Labubu Marrom', 30.00, '/img/prod2.jpeg', 10),
+('Kit Bobbie Goods + 120 canetas', 'Kit Bobbie Goods + 120 canetas ', 109.90, '/img/prod3.webp', 20),
+('Pop it', 'Pop it colorido Among Us', 19.99, '/img/prod4.webp', 15),
+('Fidget Spiner', 'Brinquedo Fidget Spinner verde', 8.00, '/img/prod5.webp', 25),
+('Boneco José Comilão', 'Boneco José Comilão', 94.40, '/img/prod6.webp', 30);
 
 select * from produtos;
 select * from pedido;
